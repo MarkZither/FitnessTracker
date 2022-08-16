61002b49c917358a85f5b56d5f724e9a38f8b11d
MarkZither <mark.burton@zither-it.co.uk>
2022-08-16 21:35:06 +0200

    add a red line to learn simple drawing

--- src/FitnessTracker.Maui/MainPage.xaml.cs
+++ src/FitnessTracker.Maui/MainPage.xaml.cs
@@ -1,9 +1,17 @@
 ﻿using Mapsui;
 using Mapsui.Extensions;
+using Mapsui.Layers;
+using Mapsui.Nts;
+using Mapsui.Nts.Extensions;
 using Mapsui.Projections;
+using Mapsui.Styles;
 using Mapsui.Widgets;
 using Mapsui.Widgets.ScaleBar;
 using Mapsui.Widgets.Zoom;
+
+using NetTopologySuite.Geometries;
+
+using NetTopologySuite.IO;
 
 namespace FitnessTracker.Maui
 {
@@ -16,7 +24,7 @@ namespace FitnessTracker.Maui
             InitializeComponent();
 
             var mapControl = new Mapsui.UI.Maui.MapControl();
-            mapControl.Map?.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
+            mapControl.Map?.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer("Marks.Fitness.MAUI.App"));
             // Get the lon lat coordinates from somewhere (Mapsui can not help you there)
             var whereYouAre = new MPoint(6.092523, 49.621953);
             // OSM uses spherical mercator coordinates. So transform the lon lat coordinates to spherical mercator
@@ -26,8 +34,37 @@ namespace FitnessTracker.Maui
             mapControl.Map.Home = n => n.NavigateTo(sphericalMercatorCoordinate, mapControl.Map.Resolutions[15]);
             mapControl.Map.Widgets.Add(new ScaleBarWidget(mapControl.Map) { TextAlignment = Alignment.Center, HorizontalAlignment = Mapsui.Widgets.HorizontalAlignment.Center, VerticalAlignment = Mapsui.Widgets.VerticalAlignment.Top });
             mapControl.Map.Widgets.Add(new ZoomInOutWidget { MarginX = 20, MarginY = 40 });
+            var lineStringLayer = CreateLineStringLayer(CreateLineStringStyle());
+            mapControl.Map.Layers.Add(lineStringLayer);
             CntViewMap.Content = mapControl;
         }
+
+        public static IStyle CreateLineStringStyle()
+        {
+            return new VectorStyle
+            {
+                Fill = null,
+                Outline = null,
+#pragma warning disable CS8670 // Object or collection initializer implicitly dereferences possibly null member.
+                Line = { Color = Mapsui.Styles.Color.FromString("Red"), Width = 8 }
+            };
+        }
+
+        public static ILayer CreateLineStringLayer(IStyle? style = null)
+        {
+            var lineString = (LineString)new WKTReader().Read(WKTGr5);
+            lineString = new LineString(lineString.Coordinates.Select(v => SphericalMercator.FromLonLat(v.Y, v.X).ToCoordinate()).ToArray());
+
+            return new MemoryLayer
+            {
+                Features = new[] { new GeometryFeature { Geometry = lineString } },
+                Name = "LineStringLayer",
+                Style = style
+
+            };
+        }
+
+        private const string WKTGr5 = "LINESTRING(49.621953 6.092523, 49.622953 6.093523, 49.623953 6.094523)";
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
