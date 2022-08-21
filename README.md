152bb34a3f7e74fdde5c2a3f6b45cbb3c1965fa1
MarkZither <mark.burton@zither-it.co.uk>
2022-08-21 18:36:08 +0200

    setup location permissions for android and windows

--- README.md
+++ README.md
@@ -3,6 +3,25 @@
 ## MVVM
 https://docs.microsoft.com/en-us/dotnet/communitytoolkit/mvvm/
 
+## Essentials
+Getting started with MAUI essentials
+https://docs.microsoft.com/en-us/xamarin/essentials/get-started?tabs=windows%2Candroid
+```
+ protected override void OnCreate(Bundle? savedInstanceState)
+ {
+     Microsoft.Maui.ApplicationModel.Platform.Init(this, savedInstanceState); // add this line to your code, it may also be called: bundle
+
+     base.OnCreate(savedInstanceState);
+ }
+
+ public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+ {
+     Microsoft.Maui.ApplicationModel.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+     base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+ }
+```
+
 ## Geolocation
 https://github.com/dotnet/maui/blob/main/src/Essentials/samples/Samples/ViewModel/GeolocationViewModel.cs
 
--- src/FitnessTracker.Maui/Platforms/Android/MainActivity.cs
+++ src/FitnessTracker.Maui/Platforms/Android/MainActivity.cs
@@ -7,5 +7,18 @@ namespace FitnessTracker.Maui
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            Microsoft.Maui.ApplicationModel.Platform.Init(this, savedInstanceState); // add this line to your code, it may also be called: bundle
+
+            base.OnCreate(savedInstanceState);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            Microsoft.Maui.ApplicationModel.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 }
