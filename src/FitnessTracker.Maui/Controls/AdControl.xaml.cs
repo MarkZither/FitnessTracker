e5dbd5c3cae162d4a9c67b53a3b1f0c120f1c85e
MarkZither <mark.burton@zither-it.co.uk>
2022-08-25 22:00:45 +0200

    more steps to platform specific control

---
+++ src/FitnessTracker.Maui/Controls/AdControl.xaml.cs
@@ -0,0 +1,28 @@
+using Microsoft.Maui.Platform;
+
+namespace FitnessTracker.Maui.Controls;
+
+public partial class AdControl : ContentView
+{
+	public AdControl()
+	{
+		InitializeComponent();
+        ModifyAd();
+    }
+
+    private void ModifyAd()
+    {
+        Microsoft.Maui.Handlers.ContentViewHandler.Mapper.AppendToMapping("MyCustomization", (handler, view) =>
+        {
+#if ANDROID
+            handler.PlatformView.SetBackgroundColor(Colors.Transparent.ToPlatform());
+#elif IOS
+            handler.PlatformView.SetBackgroundColor(Colors.Green.ToPlatform());
+            handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#elif WINDOWS
+            //handler.PlatformView.SetBackgroundColor(Colors.Blue.ToPlatform());
+            //handler.PlatformView.FontWeight = Microsoft.UI.Text.FontWeights.Thin;
+#endif
+        });
+    }
+}
---
+++ src/FitnessTracker.Maui/Controls/IAdControl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessTracker.Maui.Controls
+{
+    public interface IAdControl : IContentView
+    {
+        public string Text { get; }
+        public Color TextColor { get; }
+    }
+}
